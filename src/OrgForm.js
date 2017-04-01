f0db1d4fb2875cf5c0b6bae690dd4f5dd74a488e
Katie McGinley <katie@mcgnly.com>
2017-04-01 14:16:09 +0200

    inputs started

--- src/OrgForm.js
+++ src/OrgForm.js
@@ -19,7 +19,17 @@ export default ({ }) => {
       </ButtonToolbar>
     </section>
 		<section className="col-xs-12">
-			
+
+<div class="input-group">
+	<h3>What is your organization called?</h3>
+  <input type="text" class="form-control" placeholder="Organization Name" />
+  <h3>Describe the mission- how are you saving the world?</h3>
+  <input type="text" class="form-control" placeholder="Describe your mission" />
+  <h3>What Kind of task do you need help with? Volunteers will be able to choose what they are most skilled at or willing to do. </h3>
+  <input type="text" class="form-control" placeholder="task" />
+</div>
+
+
            <Link to="/thanks">
                   <Button bsStyle="primary">Submit your organization's task for review</Button>
            </Link>
