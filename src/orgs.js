98f45bc40526737a0ee5b40a08ee6636620bbb92
Katie McGinley <katie@mcgnly.com>
2017-04-01 12:26:19 +0200

    dataset

---
+++ src/orgs.js
@@ -0,0 +1,70 @@
+export default function() {
+
+return [
+                {
+                    name: "Maneo",
+                    logo: "http://www.maneo.de/fileadmin/template/images/maneo_logo.png",
+                    mission: "MANEO - Das schwule Anti-Gewalt-Projekt in Berlin",
+                    field_of_work: "Webdevelopment",
+                    description: "Help us maintain our website and webservers",
+                    contact: "maneo@maneo.de"
+                },
+                {
+                    name: "Maneo",
+                    logo: "http://www.maneo.de/fileadmin/template/images/maneo_logo.png",
+                    mission: "MANEO - Das schwule Anti-Gewalt-Projekt in Berlin",
+                    field_of_work: "PR", 
+                    description: "Write arcticles and formulate press statments and update our blog",
+                    contact: "maneo@maneo.de"
+                },
+                {
+                    name: "Maneo",
+                    logo: "http://www.maneo.de/fileadmin/template/images/maneo_logo.png",
+                    mission: "MANEO - Das schwule Anti-Gewalt-Projekt in Berlin",
+                    field_of_work: "Event support", 
+                    description: "Provide information to visitors",
+                    contact: "maneo@maneo.de"
+                },
+                {
+                    name: "Lamdba",
+                    logo: "https://www.lambda-bb.de/wp-content/uploads/2013/01/lambda_logo4.png",
+                    mission: "Lambda setzt auf Kooperation in der LSBTIQ* Community und in der Jugendarbeit.",
+                    field_of_work: "Event support",
+                    description: "Help out at our annual youth camp",
+                    contact: "info@lambda-bb.de "
+                },
+                {
+                    name: "Lamdba",
+                    logo: "https://www.lambda-bb.de/wp-content/uploads/2013/01/lambda_logo4.png",
+                    mission: "Lambda setzt auf Kooperation in der LSBTIQ* Community und in der Jugendarbeit.",
+                    field_of_work: "Mentorship",
+                    description: "Lead a youth group",
+                    contact: "info@lambda-bb.de "
+                },
+                {
+                    name: "Lamdba",
+                    logo: "https://www.lambda-bb.de/wp-content/uploads/2013/01/lambda_logo4.png",
+                    mission: "Lambda setzt auf Kooperation in der LSBTIQ* Community und in der Jugendarbeit.",
+                    field_of_work: "Gardening",
+                    description: "Grow weed in our beautiful garden",
+                    contact: "info@lambda-bb.de "
+                },
+                {
+                    name: "Unicorns in tech",
+                    logo: "https://media.giphy.com/media/xUPGcG2KEai6it91hm/giphy.gif",
+                    mission: "Church of Stuart",
+                    field_of_work: "Webdevelopment",
+                    description: "Help spread the gospel of Stuart",
+                    contact: "grindr://stuart_uit"
+                },
+                {
+                    name: "Unicorns in tech",
+                    logo: "https://media.giphy.com/media/xUPGcG2KEai6it91hm/giphy.gif",
+                    mission: "Church of Stuart",
+                    field_of_work: "Evangelist",
+                    description: "Be our San Francisco Pastor",
+                    contact: "grindr://stuart_uit"
+                }
+    ]
+
+};
