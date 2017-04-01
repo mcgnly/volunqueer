e6b80051744322b65c70928bc6a1610761db2ff1
Katie McGinley <katie@mcgnly.com>
2017-04-01 16:33:08 +0200

    add email form

--- src/orgList.js
+++ src/orgList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Navbar, Jumbotron, Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import { OrgCard } from './OrgCard.js';
 import orgs from './orgs.js';
 
@@ -12,14 +12,19 @@ let iterator = 0;
 	let makeTaskCards =
 		orgsData.map((task) => {
 			iterator += 1;
-
-			if (selectedTags.indexOf(task.field_of_work) !== -1) {
-				return (
-					<OrgCard key={iterator} org={task}/>
-				);
+			if (selectedTags.length < 1) {
+				return <OrgCard key={iterator} org={task}/>
 			} else {
-				return;
+				if (selectedTags.indexOf(task.field_of_work) !== -1) {
+					return (
+						<OrgCard key={iterator} org={task}/>
+					);
+				} else {
+					return;
+				}
+				
 			}
+
 	});
 
     return (
@@ -28,7 +33,29 @@ let iterator = 0;
 		<ul>
 		{makeTaskCards}
 		</ul>
-
+		<section style={
+		    {marginTop: '30px',
+		        paddingTop: '30px',
+		        borderTop: '1px solid'}
+		}>
+		<h4>Didn't find what you were looking for?</h4>
+		<form>
+	        <FormGroup
+	          controlId="formBasicText"
+	        >
+	        	<p>add your email here and we will send you a weekly digest of new organizations and tasks that match your selected interests:</p>
+	          <ControlLabel>email:</ControlLabel>
+	          <FormControl
+	            type="text"
+	            placeholder="Where can we reach you?"
+	          />
+	        </FormGroup>
+	    </form>
+	    
+	    <Link to="/volunteerThanks">
+                  <Button bsStyle="primary">Sign me up!</Button>
+        </Link>
+        </section>
 	</div>
     );
 };
