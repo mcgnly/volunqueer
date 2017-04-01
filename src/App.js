4f0fccc5454e5822e9d1e2c7109239742e000674
Katie McGinley <katie@mcgnly.com>
2017-04-01 09:05:47 +0200

    initial commit, with react-bootstrap

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <Button
+          bsStyle="success"
+          bsSize="large"
+          href="http://react-bootstrap.github.io/components.html"
+          target="_blank">
+          View React Bootstrap Docs
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
