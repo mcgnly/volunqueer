e799172b3ac4e6b3b095def066fafb8f6380c68e
Itamar Givon <itamar.givon.dev@gmail.com>
2017-04-01 14:17:47 +0300

    Add BS to home view

--- src/home.js
+++ src/home.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Button, ButtonToolbar } from 'react-bootstrap';
+
+import './home.css';
 // import { OrgPg } from './orgcard.js'
 
 
 export default ({selectedTags, addTag}) => {
     return (
-	<div>
-		<h2>Welcome to VolunQueer- what are you interested in?</h2>
-
-		<ul>
-			<li id="cause1" onClick={(ev)=>{addTag(ev.target.id)}}> some kind of tag thing </li>
-			<li id="cause2" onClick={(ev)=>{addTag(ev.target.id)}}> some kind of tag thing </li>
-			<li id="cause3" onClick={(ev)=>{addTag(ev.target.id)}}> some kind of tag thing </li>
-		</ul>
-			Find an organization
-	</div>
+	<article className="text-center">
+    <section className="lead-cta col-xs-12">
+      <h2>Contribute much?</h2>
+      <p className="lead">Pick a few things to help the community out with</p>
+    </section>
+    <section className="col-xs-12">
+      <ButtonToolbar className="skills-container">
+        <Button id="cause-1" onClick={(ev)=>{addTag(ev.target.id)}}>field 1</Button>
+        <Button id="cause-2" onClick={(ev)=>{addTag(ev.target.id)}}>field 2</Button>
+        <Button id="cause-3" onClick={(ev)=>{addTag(ev.target.id)}}>field 3</Button>
+      </ButtonToolbar>
+    </section>
+		<section className="col-xs-12">
+      <Button bsStyle="primary">Find an organization</Button>
+    </section>
+	</article>
     );
 }
 
