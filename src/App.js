6f713f08148449b443c55bfa29ac10e7fcdf63ad
Itamar Givon <itamar.givon.dev@gmail.com>
2017-04-01 12:34:35 +0300

    Cleanup app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import {
   BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom'
+} from 'react-router-dom';
 import logo from './unicorn.png';
 import './App.css';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
-import Home from './home.js';
 import TopContainer from './TopContainer';
 
 
@@ -19,68 +17,12 @@ const App = () => (
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Volunqueer</h2>
         </div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
 
       <hr/>
 
       <Route exact path="/" component={TopContainer}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
     </div>
   </Router>
-)
-
-
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-        <Button
-          bsStyle="success"
-          bsSize="large"
-          href="http://react-bootstrap.github.io/components.html"
-          target="_blank">
-          View React Bootstrap Docs
-        </Button>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
 )
 
 export default App;
