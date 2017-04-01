7d1dd7524d34aaab763ac28285b8d94560c48d4c
Katie McGinley <katie@mcgnly.com>
2017-04-01 12:49:55 +0200

    rename to avoid problems with hyphen in object property

--- src/OrgCard.js
+++ src/OrgCard.js
@@ -5,7 +5,7 @@ export class OrgCard extends Component {
   constructor(props) {
     super(props);
     this.org = this.props.org
-    console.log(this.props);
+    this.org.work = this.props.org['field-of-work']
   }
 
 
@@ -18,8 +18,8 @@ export class OrgCard extends Component {
         <div className="org-info">
           <h2 className="org-name">{this.org.name}</h2>
           <p className="org-mission">{this.org.mission}</p>
-          <p className="need-descritpion">this.org.description</p>
-          <p className="need-field">this.org.field-of-work</p>
+          <p className="need-descritpion">{this.org.description}</p>
+          <p className="need-field">{this.org.work}</p>
           <p className="org-cta"><a href={"mailto:" + this.org.contact}>Apply</a></p>
         </div>
       </div>
