db7a3b4c2567cd4d3b029fde5ee8d8973d78ca3c
Katie McGinley <katie@mcgnly.com>
2017-04-01 17:42:06 +0200

    margin over the logo

--- src/OrgCard.js
+++ src/OrgCard.js
@@ -14,10 +14,10 @@ export class OrgCard extends Component {
   render() {
     return (
       <div className="org-card">
-        <div className="org-logo col-md-3 text-center">
+        <div className="org-logo col-sm-3 text-center">
           <img className="img-responsive img-thumbnail" src={this.org.logo} alt={this.org.name} />
         </div>
-        <div className="org-info col-md-9">
+        <div className="org-info col-sm-9">
           <h2 className="org-name">{this.org.name}</h2>
           <p className="org-mission">{this.org.mission}</p>
           <p className="need-descritpion">{this.org.description}</p>
