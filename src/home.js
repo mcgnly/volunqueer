c30a403bc3244686041c82841475ac5aed342741
Katie McGinley <katie@mcgnly.com>
2017-04-01 12:43:40 +0200

    auto generate tags from son data

--- src/home.js
+++ src/home.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
-// import { OrgPg } from './orgcard.js'
+import orgs from './orgs.js';
 
+export default ({ selectedTags, addTag }) => {
 
-export default ({selectedTags, addTag}) => {
+	let orgsData = orgs();
+	let iterator = 0;
+	let alreadyUsed = [];
+	let makeTags =
+		orgsData.map((task) => {
+			iterator+=1;
+			if (alreadyUsed.indexOf(task.field_of_work)===-1){
+				alreadyUsed.push(task.field_of_work);
+				return (
+					<li key={iterator} id={task.field_of_work} onClick={(ev) => { addTag(ev.target.id); } }> {task.field_of_work} </li>
+				);
+			} else {
+				return;
+			}
+	});
+
     return (
 	<div>
 		<h2>Welcome to VolunQueer- what are you interested in?</h2>
 
 		<ul>
-			<li id="cause1" onClick={(ev)=>{addTag(ev.target.id)}}> some kind of tag thing </li>
-			<li id="cause2" onClick={(ev)=>{addTag(ev.target.id)}}> some kind of tag thing </li>
-			<li id="cause3" onClick={(ev)=>{addTag(ev.target.id)}}> some kind of tag thing </li>
+			{makeTags}
 		</ul>
 
 		<li><Link to="/topics">Find an organization</Link></li>
-			
+
 	</div>
     );
-}
-
-//TODO: don't open link in new tab???
+};
